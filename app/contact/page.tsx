c7085c0108fe055c87197974e360ecf786cf2c98
Shalom Ademuwagun <38067923+ChachyDev@users.noreply.github.com>
2024-06-04 20:37:00 +0100

    fix(contact): update email

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
                             <p className="text-neutral-500 pt-8">
                                 You can contact us at
                                 <a href="mailto:placeholder@nexcode.org"
-                                   className="text-primary hover:underline hover:text-primary-100"> placeholder@nexcode.org</a>
+                                   className="text-primary hover:underline hover:text-primary-100"> contact@nexcode.org</a>
                             </p>
                             <p className="pt-2 text-lg">
                                 Nexcode Limited is a company registered in England and Wales with company number 14665873 under the Companies Act 2006.
